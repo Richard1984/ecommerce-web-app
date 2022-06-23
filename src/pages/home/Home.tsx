6eeb30435e898aeb45bc213cdf48923e9bc628d4
Riccardo Sangiorgio <richard.sangiorgio@gmail.com>
2022-06-23 16:36:35 +0200

    Router.

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,12 @@
+
+const Home = () => {
+
+    return (
+        <div>
+            <h1>Home</h1>
+            <a href="/login">Login</a>
+        </div>
+    )
+}
+
+export default Home
---
+++ src/pages/login/Login.tsx
@@ -0,0 +1,12 @@
+
+const Login = () => {
+
+    return (
+        <div>
+            <h1>Login</h1>
+            <a href="/">Home</a>
+        </div>
+    )
+}
+
+export default Login
