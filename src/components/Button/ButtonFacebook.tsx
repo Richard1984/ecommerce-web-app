246fac0fcfc213162da31e24363fa60559faac36
Riccardo Sangiorgio <richard.sangiorgio@gmail.com>
2022-07-12 15:13:26 +0200

    Corretto errore ButtonFacebook.

--- src/components/Button/ButtonFacebook.tsx
+++ src/components/Button/ButtonFacebook.tsx
@@ -1,11 +1,12 @@
 import { faFacebook } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Button, { ButtonProps } from "./Button";
+import styles from './button.module.scss';
 
 interface ButtonFacebookProps extends ButtonProps { }
 
 const ButtonFacebook = (props: ButtonFacebookProps) => {
-    return (<Button {...props} className="button--facebook" leftIcon={<FontAwesomeIcon icon={faFacebook} />} text="Entra con Facebook"/>)
+    return (<Button {...props} className={styles["button--facebook"]} leftIcon={<FontAwesomeIcon icon={faFacebook} />} text="Entra con Facebook"/>)
 }
 
 export default ButtonFacebook
