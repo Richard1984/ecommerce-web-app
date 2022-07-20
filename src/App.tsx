a10400a3160301780882e5b5b9cffaee7701c8d4
Riccardo Sangiorgio <richard.sangiorgio@gmail.com>
2022-07-20 16:46:08 +0200

    Routes modificate.

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,8 @@ import Payment from './pages/payment/Payment';
 import ProductDetail from './pages/product-detail/ProductDetail';
 import SearchRoute from './pages/search/Search';
 import Shop from './pages/shop/Shop';
-import ShoppingListsRoute from './pages/shopping-lists/ShoppingLists';
+import ShoppingListProductsRoute from './pages/shopping-lists/pages/ShoppingListProducts/ShoppingListProducts';
+import ShoppingListsRoute from './pages/shopping-lists/pages/ShoppingLists/ShoppingLists';
 import SignUp from './pages/signup/Signup';
 import { getAccount } from './reducers/authentication';
 import UserRoleEnum from './shared/enums/role.enum';
@@ -47,33 +48,42 @@ function App() {
         limit={3}
       />
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route element={<PublicLayout />}>
-          {/* PUBLIC PATHS */}
-          <Route index element={<Home />} />
-          <Route path="/search" element={<SearchRoute />} />
-          <Route path="/products/:id" element={<ProductDetail />} />
-          <Route path="/categories/:id/products" element={<CategoryProductsRoute />} />
+        <Route path="/">
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<SignUp />} />
+          <Route element={<PublicLayout />}>
+            {/* PUBLIC PATHS */}
+            <Route index element={<Home />} />
+            <Route path="search" element={<SearchRoute />} />
+            <Route path="products/:id" element={<ProductDetail />} />
+            <Route path="categories/:id/products" element={<CategoryProductsRoute />} />
 
-          {/* LOGGED USER PATHS */}
-          <Route path="/account" element={<PrivateRoute><Account /></PrivateRoute>} />
-          <Route path="/account/edit" element={<PrivateRoute><EditProfileRoute /></PrivateRoute>} />
-          <Route path="/account/cart" element={<PrivateRoute><Cart /></PrivateRoute>} />
-          <Route path="/account/lists" element={<PrivateRoute><ShoppingListsRoute /></PrivateRoute>} />
-          <Route path="/account/orders" element={<PrivateRoute><Orders /></PrivateRoute>} />
-          <Route path="/account/orders/:id" element={<PrivateRoute><OrderDetail /></PrivateRoute>} />
-          <Route path="/account/payment_methods" element={<PrivateRoute><PaymentMethods /></PrivateRoute>} />
-          <Route path="/account/payment_methods/new" element={<PrivateRoute><NewPaymentMethod /></PrivateRoute>} />
+            {/* LOGGED USER PATHS */}
+            <Route path="account" element={<PrivateRoute/>}>
+              <Route index element={<Account />} />
+              <Route path="edit" element={<EditProfileRoute />} />
+              <Route path="cart" element={<Cart />} />
+              <Route path="lists">
+                <Route path=":id/products" element={<ShoppingListProductsRoute />} />
+                <Route index element={<ShoppingListsRoute />} />
+              </Route>
+              <Route path="orders" element={<Orders />} />
+              <Route path="orders/:id" element={<OrderDetail />} />
+              <Route path="payment_methods" element={<PaymentMethods />} />
+              <Route path="payment_methods/new" element={<NewPaymentMethod />} />
+            </Route>
 
-          {/* LOGGED ADMIN PATHS */}
-          <Route path="/admin/shop" element={<PrivateRoute hasAnyAuthorities={[UserRoleEnum.ADMIN]}><Shop /></PrivateRoute>} />
-          <Route path="/admin/shop/edit" element={<PrivateRoute hasAnyAuthorities={[UserRoleEnum.ADMIN]}><EditShopRoute /></PrivateRoute>} />
-          <Route path="/admin/products/new" element={<PrivateRoute hasAnyAuthorities={[UserRoleEnum.ADMIN]}><EditProductRoute /></PrivateRoute>} />
-          <Route path="/admin/products/:id/edit" element={<PrivateRoute hasAnyAuthorities={[UserRoleEnum.ADMIN]}><EditProductRoute /></PrivateRoute>} />
-          <Route path="/admin/categories" element={<PrivateRoute hasAnyAuthorities={[UserRoleEnum.ADMIN]}><Categories /></PrivateRoute>} />
+            {/* LOGGED ADMIN PATHS */}
+            <Route path="admin" element={<PrivateRoute hasAnyAuthorities={[UserRoleEnum.ADMIN]}/>}>
+              <Route path="shop" element={<Shop />} />
+              <Route path="shop/edit" element={<EditShopRoute />} />
+              <Route path="products/new" element={<EditProductRoute />} />
+              <Route path="products/:id/edit" element={<EditProductRoute />} />
+              <Route path="categories" element={<Categories />} />
+            </Route>
+          </Route>
+          <Route path="payment" element={<PrivateRoute><Payment /></PrivateRoute>} />
         </Route>
-        <Route path="/payment" element={<PrivateRoute><Payment /></PrivateRoute>} />
       </Routes>
     </div>
   );
